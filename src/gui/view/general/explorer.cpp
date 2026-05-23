1c96813d1b4032c73fd5c8e1d3bf30df620013dc
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2026-05-23 12:11:49 +0800

    Fixed indent issue

--- src/gui/view/general/explorer.cpp
+++ src/gui/view/general/explorer.cpp
@@ -271,6 +271,7 @@ namespace January::Engine::View {
     }
 
     void JViewExplorer::DrawLeftSideTreeNode(JFolderContent& tree, int32_t level){
+        ImGui::PushStyleVar(ImGuiStyleVar_IndentSpacing, 8.0f);
         if(ImGui::TreeNodeEx((tree.name + "##Exploere_Left_Panel_Tree_Node_" + std::to_string(level)).c_str())){
             if (ImGui::IsItemToggledOpen()) {
                 {
@@ -297,6 +298,7 @@ namespace January::Engine::View {
             }
             ImGui::TreePop();
         }
+        ImGui::PopStyleVar();
     }
 
     void JViewExplorer::DrawLeftSide(){
