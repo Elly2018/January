cec3019059e672d6507824f1594eb8f141cf9f61
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2026-05-29 17:10:12 +0800

    fix glitch file issue

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -151,6 +151,8 @@ namespace January::Engine {
         ImGuiIO& io = ImGui::GetIO(); (void)io;
         ImFontConfig font_cfg;
         font_cfg.FontDataOwnedByAtlas = false;
+        std::string save_path = Engine::get_config_path("imgui.ini").string();
+        io.IniFilename = save_path.c_str();
         jengine.context->text_font = io.Fonts->AddFontFromFileTTF("Roboto-Medium.ttf", 16.0f, &font_cfg);
         ImGui::MergeIconsWithLatestFont(16.f, false);
         jengine.context->icon_font = io.Fonts->AddFontFromFileTTF("icons.ttf", 16.0f, &font_cfg);
